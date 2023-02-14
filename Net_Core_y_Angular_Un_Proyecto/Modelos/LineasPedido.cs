a727e3941a6f3f4cd476bfa3ff73bc0150878e6a
Victor <VictorCarmona9035@hotmail.com>
2023-02-14 21:54:43 +0100

    11-Conectamos a la base de datos con Entuty Framework

---
+++ Net_Core_y_Angular_Un_Proyecto/Modelos/LineasPedido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Net_Core_y_Angular_Un_Proyecto.Modelos
+{
+    public partial class LineasPedido
+    {
+        public int Id { get; set; }
+        public int IdPedido { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal ImporteUnitario { get; set; }
+
+        public virtual Pedido IdPedidoNavigation { get; set; } = null!;
+        public virtual Producto IdProductoNavigation { get; set; } = null!;
+    }
+}
---
+++ Net_Core_y_Angular_Un_Proyecto/Modelos/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Net_Core_y_Angular_Un_Proyecto.Modelos
+{
+    public partial class Producto
+    {
+        public Producto()
+        {
+            LineasPedidos = new HashSet<LineasPedido>();
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public decimal Precio { get; set; }
+        public string Descripcion { get; set; } = null!;
+
+        public virtual ICollection<LineasPedido> LineasPedidos { get; set; }
+    }
+}
